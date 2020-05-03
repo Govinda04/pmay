4b8220ac83d7914264b8bf8c4983a4a02372a6e9
Govinda04 <govindakocharekar@gmail.com>
2020-05-03 13:40:07 +0530

    carousel

--- js/src.js
+++ js/src.js
@@ -1,7 +1,25 @@
 $(document).ready(function () {
-    $('#navId a').click(e => {
-        e.preventDefault();
-        $(this).tab('show');
-    });
-    
+  $("#navId a").click((e) => {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+
+  $("#main_car").owlCarousel({
+    loop: true,
+    margin: 10,
+    rewind: true,
+    autoplay: true,
+    // nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
 });
