2802153caa6aad11cf94a6a9e5b0ad33f18354af
govinda04 <govinda@boppotechnologies.com>
2020-05-03 19:58:46 +0530

    navbar fixed

--- js/src.js
+++ js/src.js
@@ -4,25 +4,6 @@ $(document).ready(function () {
     $(this).tab("show");
   });
 
-  $("#main_car").owlCarousel({
-    loop: true,
-    margin: 10,
-    rewind: true,
-    autoplay: true,
-    // nav: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      600: {
-        items: 3,
-      },
-      1000: {
-        items: 3,
-      },
-    },
-  });
-
   $("#quiz_form").on("submit", function (e) {
     e.preventDefault();
     $(".pmay_quiz_result").hide();
